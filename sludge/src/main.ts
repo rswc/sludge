f674b54424877f4cf07471da8c8a5469d7bf18e2
rswc <rshoryti@protonmail.com>
2022-12-25 13:38:18 +0100

    Add Bootstrap

--- sludge/src/main.ts
+++ sludge/src/main.ts
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/bootstrap.scss'
+import * as bootstrap from 'bootstrap'
+
 import './assets/main.css'
 
 const app = createApp(App)
