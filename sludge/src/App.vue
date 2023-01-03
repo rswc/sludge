f2052844dcf9df3c324d470953a540f795d8788b
rswc <rshoryti@protonmail.com>
2023-01-03 17:38:18 +0100

    Remove old About page

--- sludge/src/App.vue
+++ sludge/src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-    </div>
-  </header> -->
-
   <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/events">Event Log</RouterLink>
@@ -22,7 +12,6 @@ import HelloWorld from './components/HelloWorld.vue'
     <RouterLink to="/groups">Groups</RouterLink>
     <RouterLink to="/resources">Resources</RouterLink>
     <RouterLink to="/transfers">Transfers</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
   </nav>
 
   <main>
